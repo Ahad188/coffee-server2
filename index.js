4ba593d5480a06264ae10c0368d7c860b8de100f
Abu Ahad <abuahad35188@gmail.com>
2025-07-13 12:09:17 -0700

    delete route create :

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // require('dotenv').config();
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -49,6 +49,12 @@ async function run() {
       res.send(result)
     })
 
+    app.delete("/coffee/:id", async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await coffeeCollections2.deleteOne(query);
+      res.send(result)
+    })
 
 
 
