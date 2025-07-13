1ad098768b57a32b028711d74e55cea45572143d
Abu Ahad <abuahad35188@gmail.com>
2025-07-13 01:11:24 -0700

    mongodb connected

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+// require('dotenv').config();
+require('dotenv').config();
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
 const app = express();
-const port = process.env.Port || 5000;
+const port = process.env.port || 5000;
 
 app.use(cors());
 app.use(express.json());
-// RH4RHOIm62x3hIc6
-// coffeeShop2
+console.log(process.env.user)
+console.log(process.env.password)
+ 
+//  Db_user = coffeeShop2;
+// Db_password = RH4RHOIm62x3hIc6;
 
+ 
+// const uri = `mongodb+srv://coffeeShop2:RH4RHOIm62x3hIc6@cluster0.ejfmzqt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri =`mongodb+srv://${process.env.user}:${process.env.password}@cluster0.ejfmzqt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+ 
 
-const uri =` mongodb+srv:// ${process.env.Db_user}:${process.env.Db_password}@cluster0.ejfmzqt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,10 +39,11 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
 
 
 app.get('/',(req,res)=>{
